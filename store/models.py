b9a38b4c269e54385da9c8d22a9375ff8ad57b43
Ali Sadri <asadri721@gmail.com>
2022-06-12 16:33:16 +0430

    Apply migrations

--- store/models.py
+++ store/models.py
@@ -12,7 +12,8 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length= 255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
